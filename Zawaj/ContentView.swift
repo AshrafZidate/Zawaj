593360daac694d2a20a4d67c4d3239d1a92f6ae7
Ashraf Zidate <ashraf@edgeunlockedai.com>
2025-12-19 00:08:46 +0000

    Initial Commit

---
+++ Zawaj/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zawaj
+//
+//  Created by Ashraf Zidate on 19/12/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zawaj/ZawajApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZawajApp.swift
+//  Zawaj
+//
+//  Created by Ashraf Zidate on 19/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct ZawajApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
